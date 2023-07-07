bd4b55fca5cf5ac3d130dd63c89535f6dd1c1fc9
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2023-07-07 18:50:21 +0300

    add token to requests, check token expiration

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -3,15 +3,28 @@ package api
 import (
 	"errors"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/boyanivskyy/hotel-reservation/db"
+	"github.com/boyanivskyy/hotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
 	userStore db.UserStore
+}
+
+type AuthParams struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	User  *types.User `json:"user"`
+	Token string      `json:"token"`
 }
 
 func NewAuthHandler(userStore db.UserStore) *AuthHandler {
@@ -33,11 +46,33 @@ func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
 		}
 	}
 
-	if err = bcrypt.CompareHashAndPassword([]byte(user.EncryptedPassword), []byte(params.Password)); err != nil {
+	if !types.IsValidPassword(user.EncryptedPassword, params.Password) {
 		return fmt.Errorf("invalid credentials")
 	}
 
 	fmt.Println("authenticated ->", user)
 
-	return c.JSON(user)
+	return c.JSON(AuthResponse{
+		User:  user,
+		Token: createTokenFromUser(user),
+	})
+}
+
+func createTokenFromUser(user *types.User) string {
+	now := time.Now()
+	expires := now.Add(time.Hour * 4).Unix()
+	claims := jwt.MapClaims{
+		"id":      user.Id,
+		"email":   user.Email,
+		"expires": expires,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := os.Getenv("JWT_SECRET")
+	tokenStr, err := token.SignedString([]byte(secret))
+	if err != nil {
+		fmt.Println("failed to sign token with secret, error: ", err)
+	}
+
+	return tokenStr
 }
